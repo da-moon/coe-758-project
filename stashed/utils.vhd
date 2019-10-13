af82407dca25e4cfe10e01e0b71b3a13de20cd9d
damoonazarpazhooh <damoon.azarpazhooh@live.com>
2019-10-13 18:01:18 +0000

    restarted

--- stashed/utils.vhd
+++
@@ -1,7 +0,0 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.all;
-
-package utils is
-  function MAX (LEFT, RIGHT: INTEGER) return INTEGER;
-  function MIN (LEFT, RIGHT: INTEGER) return INTEGER;
-END PACKAGE;
